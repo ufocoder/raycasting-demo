6426191181b588c4073ee62bda39c8f5d63eb2bc
Sergey Ufocoder <xufocoder@gmail.com>
2025-05-25 22:17:52 +0300

    build: deploy on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solid from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/raycasting-demo/",
   plugins: [
     solid(),
     tailwindcss(),
